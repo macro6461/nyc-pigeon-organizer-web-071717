61794413a583afdce0bb27d12d184288137dd929
Matthew Croak <mattcroak718@gmail.com>
2017-07-09 17:56:57 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,27 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  #write your code here!
+  pigeon_list = {}
+
+  pigeon_names = data.collect do |attributes, characteristic|
+    characteristic.values
+  end
+  .flatten.uniq
+
+  pigeon_names.each { |name| pigeon_list[name] = {}}
+
+  data.each do |attributes, characteristic|
+    pigeon_list.each do |name, value|
+      pigeon_list[name][attributes] = []
+    end
+  end
+
+  data.each do |attributes, characteristic|
+    characteristic.each do |value, pigeon_names|
+      pigeon_names.each do |name|
+        pigeon_list[name][attributes] << value.to_s
+      end
+    end
+  end
+
+  pigeon_list
 end
